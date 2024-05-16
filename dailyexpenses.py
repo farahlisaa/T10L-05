a1ecb0c8f09d00a93caa728cd23b402de4af6e0d
Alisha <1221107133@student.mmu.edu.my>
2024-05-16 22:18:01 +0800

    identifying error

--- dailyexpenses.py
+++ dailyexpenses.py
@@ -2,6 +2,7 @@ import os
 import tkinter as tk
 from tkinter import ttk
 
+
 def add_expense():
     date = date_entry.get()
     category = category_entry.get()
@@ -71,10 +72,6 @@ amount_entry.grid(row=2, column=1, padx=5, pady=5)
 
 add_button = tk.Button(root, text="Add Expense", command=add_expense)
 add_button.grid(row=3, column=0, columnspan=2, padx=5, pady=10)
-
-categories = ['bills', 'food', 'groceries', 'shopping', 'others']
-cb1 = ttk.Combobox(root,values=categories,width=18)
-cb1.grid(row=1, column=1, padx=10, pady=10)
 
 # Create a treeview to display expenses
 columns = ("Date", "Category", "Amount")
